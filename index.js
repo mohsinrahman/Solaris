278f96bde8a9d13837b75fe49d2ee13991d7da9a
mohsin rahman <mohsinrahmang@gmail.com>
2023-11-16 00:36:38 +0100

    added few files

--- index.js
+++ index.js
@@ -25,47 +25,117 @@ class CreateSolarSystem {
             planet[index].name.style.height = planet[index].height;
             planet[index].name.style.background = planet[index].background;
             planet[index].name.style.borderRadius = planet[index].borderadius;
+            if(planet[index].id === "Saturnus"){
+                let ellipseDiv = document.createElement('div');
+                ellipseDiv.style.width = "20px";
+                ellipseDiv.style.height = "10px";
+                ellipseDiv.style.backgroundColor = "red";
+            // planet[index].name.style.borderTop: 1px solid black;
+            //     width: 150px;
+            //      rotate: -30deg;
+            //     filter: blur(2px);
+            console.log("Click Saturnus");
+            }
             console.log(planet[index].name);
         }
         for (let index = 0; index < planet.length; index++) {
             mainDiv.appendChild(planet[index].name);    
         }
-
-    
-  
-
     };
     getPlanetData();
   }
 }
 
-  const ourSolarSystem = new CreateSolarSystem();
-        ourSolarSystem.start();
+class DisplayPlanetDetails {
+    constructor() { 
+     
+     } 
 
+    async solaris() {
+        let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
+        method: "GET",
+        headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" },
+    });
+    const data = await resp.json();
+    window.solaris = data.bodies;
 
-async function Solaris() {
-    let resp = await fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
-    method: "GET",
-    headers: { "x-zocom": "solaris-1Cqgm3S6nlMechWO" },
-});
+    console.log(data);
 
-const data = await resp.json();
     
 
   
-    for(let index = 0; index < data.bodies.length; index++) {
+   for(let index = 0; index < data.bodies.length; index++) {
        
                 
-         let div1 = document.getElementById(data.bodies[index].name);
+         let clickPlanet = document.getElementById(data.bodies[index].name);
         
-         div1.addEventListener('click', function(){ console.log(data.bodies[index].name); });    
-
+         clickPlanet.addEventListener('click', function(){ 
+            
+         this.parentNode.remove()
+        //  this.parentNode.removeChild(this);
+         let mainDiv = document.createElement('div');
+        mainDiv.style.cssText = 'position:absolute;width:100%;height:100%;z-index:100;display: grid; grid-template-columns: 2fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr; grid-gap: 5px;justify-items: center; align-items: center; background: linear-gradient(to left, #0C164D 0%, #190B22 100%); padding: 10px;';
+        document.body.append(mainDiv);
+        let backToMainPage = document.createElement("button");
+        backToMainPage.style.width = 100+"px";
+        backToMainPage.style.height = 50+"px";
+        backToMainPage.innerText = "Back To Main Page";
+        backToMainPage.id = "backPage";
+        backToMainPage.style.color = "black";
+        mainDiv.appendChild(backToMainPage);
+        backToMainPage.addEventListener('click', function(){ 
+            mainDiv.remove()
+            createSolarSystem.start()
+            displayPlanetDetails.solaris(); 
+        })
+        let cardDiv = document.createElement('div');
+        cardDiv.style.width = 500+"px";
+        mainDiv.append(cardDiv);
+        mainDiv.style.color = "#ffffff";
+        let h31 = document.createElement('h3');
+        cardDiv.append(h31);
+        h31.innerHTML += data.bodies[index].name; 
+        let p = document.createElement('p');
+        cardDiv.append(p);
+        p.innerHTML += data.bodies[index].desc; 
+        let h32 = document.createElement('h3');
+        cardDiv.append(h32);
+        h32.innerText += "OMKRETS";
+        let h33 = document.createElement('h3');
+        cardDiv.append(h33);
+        h33.innerText += "KM FRÅN SOLEN";
+        let h34 = document.createElement('h3');
+        cardDiv.append(h34);
+        h34.innerText += "MAX TEMPERATUR";
+        let h35 = document.createElement('h3');
+        cardDiv.append(h35);
+        h35.innerText += "MÅNAR";
+       
+             
+            console.log(data.bodies[index].name,data.bodies[index].desc, data.bodies[index].circumference,data.bodies[index].latinName, data.bodies[index].type, data.bodies[index].distance,data.bodies[index].rotation ); });    
+         
        }
-      
-       
-       
-       
-    
+
+  }
+} 
+
+
+const createSolarSystem = new CreateSolarSystem();
+createSolarSystem.start();
+
+
+
+const displayPlanetDetails = new DisplayPlanetDetails();
+displayPlanetDetails.solaris();
+  
+
+
+
+ 
+
+
+
+  
 
 
 //    for (let index = 0; index < alldivs.length; index++) {
@@ -85,18 +155,3 @@ const data = await resp.json();
     
 //    }
     
-
-
-  }
-
-  Solaris();
-
-
-
- 
-
-
-
-  
-
-
