7e9a6abd57d6413d84c2be4c6a3fa7aff6fd4c66
mohsin rahman <mohsinrahmang@gmail.com>
2023-11-17 00:08:57 +0100

    added code

--- index.js
+++ index.js
@@ -27,9 +27,14 @@ class CreateSolarSystem {
             planet[index].name.style.borderRadius = planet[index].borderadius;
             if(planet[index].id === "Saturnus"){
                 let ellipseDiv = document.createElement('div');
-                ellipseDiv.style.width = "20px";
-                ellipseDiv.style.height = "10px";
-                ellipseDiv.style.backgroundColor = "red";
+                console.log(planet[index].name);
+                planet[index].name.append(ellipseDiv);
+                ellipseDiv.style.borderTop= "solid";
+                ellipseDiv.style.rotate= "-27deg";
+                ellipseDiv.style.filter= "blur(2px)";
+                ellipseDiv.style.width = "148px";
+                ellipseDiv.style.borderColor = "white";
+                ellipseDiv.style.marginTop="50%";
             // planet[index].name.style.borderTop: 1px solid black;
             //     width: 150px;
             //      rotate: -30deg;
@@ -74,12 +79,12 @@ class DisplayPlanetDetails {
          this.parentNode.remove()
         //  this.parentNode.removeChild(this);
          let mainDiv = document.createElement('div');
-        mainDiv.style.cssText = 'position:absolute;width:100%;height:100%;z-index:100;display: grid; grid-template-columns: 2fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr; grid-gap: 5px;justify-items: center; align-items: center; background: linear-gradient(to left, #0C164D 0%, #190B22 100%); padding: 10px;';
+        mainDiv.style.cssText = 'position:fixed;width:100%;height:100%;z-index:100;display: grid; grid-template-columns: 1fr 3fr; grid-gap: 2px;justify-items: center; align-items: center; background: linear-gradient(to left, #0C164D 0%, #190B22 100%); text-align: justify; font-family: Arial, Helvetica, sans-serif; font-size: 16px; ';
         document.body.append(mainDiv);
         let backToMainPage = document.createElement("button");
-        backToMainPage.style.width = 100+"px";
+        backToMainPage.style.width = 50+"px";
         backToMainPage.style.height = 50+"px";
-        backToMainPage.innerText = "Back To Main Page";
+        backToMainPage.innerText = "<<";
         backToMainPage.id = "backPage";
         backToMainPage.style.color = "black";
         mainDiv.appendChild(backToMainPage);
@@ -92,24 +97,68 @@ class DisplayPlanetDetails {
         cardDiv.style.width = 500+"px";
         mainDiv.append(cardDiv);
         mainDiv.style.color = "#ffffff";
-        let h31 = document.createElement('h3');
-        cardDiv.append(h31);
-        h31.innerHTML += data.bodies[index].name; 
+        let h11 = document.createElement('h1');
+        cardDiv.append(h11);
+        h11.style.textTransform = "uppercase";
+        h11.style.fontSize = "bold";
+        h11.style.letterSpacing = "5px";
+        h11.innerHTML += data.bodies[index].name;
+        let h31Latin = document.createElement('h3');
+        cardDiv.append(h31Latin);
+        h31Latin.style.textTransform = "uppercase";
+        h31Latin.style.letterSpacing = "5px";
+        h31Latin.style.color= "#FFD029";
+        h31Latin.innerHTML += data.bodies[index].latinName;  
         let p = document.createElement('p');
         cardDiv.append(p);
-        p.innerHTML += data.bodies[index].desc; 
+        cardDiv.style.letterSpacing = "1px";
+        p.innerHTML += data.bodies[index].desc;
+
         let h32 = document.createElement('h3');
         cardDiv.append(h32);
         h32.innerText += "OMKRETS";
-        let h33 = document.createElement('h3');
-        cardDiv.append(h33);
-        h33.innerText += "KM FRÅN SOLEN";
+        let p33 = document.createElement('p');
+        cardDiv.append(p33);
+        p33.innerHTML += data.bodies[index].circumference +" KM";
+
         let h34 = document.createElement('h3');
         cardDiv.append(h34);
-        h34.innerText += "MAX TEMPERATUR";
-        let h35 = document.createElement('h3');
-        cardDiv.append(h35);
-        h35.innerText += "MÅNAR";
+        h34.innerText += "KM FRÅN SOLEN";
+        let p35 = document.createElement('p');
+        cardDiv.append(p35);
+        p35.innerHTML += data.bodies[index].distance +" KM";
+
+        let h36 = document.createElement('h3');
+        cardDiv.append(h36);
+        h36.innerText += "MAX TEMPERATUR";
+        let p37 = document.createElement('p');
+        cardDiv.append(p37);
+        p37.innerHTML += data.bodies[index].temp.day +"C";
+
+        let h38 = document.createElement('h3');
+        cardDiv.append(h38);
+        h38.innerText += "MIN TEMPERATUR";
+        let p39 = document.createElement('p');
+        cardDiv.append(p39);
+        p39.innerHTML += data.bodies[index].temp.night +"C";
+
+        let h311 = document.createElement('h3');
+        cardDiv.append(h311);
+        h311.innerText += "MÅNAR";
+        let p312 = document.createElement('p');
+        cardDiv.append(p312);
+        if(data.bodies[index].moons.length > 0){
+            console.log(data.bodies[index].moons.length);
+            for (let i = 0; i < data.bodies[index].moons.length; i++) {
+                
+                p312.innerHTML += data.bodies[index].moons[i] + " - ";                
+            }
+           
+        }
+        else {
+            p312.innerHTML += "NA";
+        }
+        
        
              
             console.log(data.bodies[index].name,data.bodies[index].desc, data.bodies[index].circumference,data.bodies[index].latinName, data.bodies[index].type, data.bodies[index].distance,data.bodies[index].rotation ); });    
